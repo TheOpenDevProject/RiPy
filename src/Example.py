c0b3633a27e048000e6b6c67a0f55428b421cbe4
TheOpenDevProject <tkgs28@gmail.com>
2014-11-25 14:27:52 +1100

    API Key Removed
    API Key was accidently added with source, key has been removed from
    account.

--- src/Example.py
+++ src/Example.py
@@ -1,6 +1,6 @@
 from RiPy.BasicProfile import BasicProfileAPI
 
-profile = BasicProfileAPI("eb84c1c1-d008-48a8-aaf9-042b5ba4ba2a","oce")
+profile = BasicProfileAPI("APIKey","oce")
 
 profile.requestSummonerProfile("SumCoolAid")
 
